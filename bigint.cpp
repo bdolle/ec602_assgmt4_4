67a37a80dc40893d6f99c6bb09d16613704290a2
bdolle <43939940+bdolle@users.noreply.github.com>
2020-10-22 23:19:49 -0400

    Update bigint.cpp

--- bigint.cpp
+++ bigint.cpp
@@ -1,8 +1,21 @@
+// Copyright 2020 Brian Dolle bdolle@bu.edu
+// Copyright 2020 Poras Shroff pshroff4@bu.edu
+
 #include"bigint.h"
-#include<iostream>
+// #include<iostream>
 #include<vector>
 #include<string>
-#include<cmath>
+
+BigInt reverse_fxn(BigInt a){
+  int len = a.length();
+  int n = len-1;
+  BigInt b = a;
+  for(int i=0; i<(len/2);i++){
+    std::swap(b.at(i),b.at(n));
+    n=n-1;
+  }
+  return b;
+}
 
 
 BigInt multiply_int(const BigInt &a, const BigInt &b){
@@ -10,37 +23,79 @@ BigInt multiply_int(const BigInt &a, const BigInt &b){
   int len_d = static_cast<int>(a.size()+b.size()-1);
   std::vector<int> d(len_d,0);
 
-  int temp = 0;
+  BigInt a_prime = reverse_fxn(a);
+  BigInt b_prime = reverse_fxn(b);
+
+  int value = 0;
   int digit = 0;
   int carry = 0;
 
-  for(int i=0; i<a.size(); i++){
-    for(int j=0; j<b.size(); j++){
-      temp = (a.at(i)-'0')*(b.at(i)-'0')+carry;
-      digit = temp % 10;
-      carry = floor(temp / 10);
-      d.at(i+j) = d.at(i+j)+digit;
+  for(int i=0; i<a_prime.size(); i++){
+    for(int j=0; j<b_prime.size(); j++){
+       d.at(i+j) = d.at(i+j) + (a_prime.at(i)-'0')*(b_prime.at(j)-'0');
     }
   }
-  
+
+  std::vector<int> ans(len_d,0);
+  for(int j=0; j<d.size(); j++){
+    value = d.at(j) + carry;
+    digit = value % 10;
+    ans.at(j) = digit;
+    carry = value/10;
+  }
+
   if(carry > 0){
-    d.push_back(carry);
+    ans.push_back(carry);
   }
 
-  BigInt c(d.size(),0);
+  BigInt c(ans.size(),0);
   for(int i=0; i<c.size(); i++){
-    c.at(i) = d.at(i)+'0';
-    std::cout << c.at(i) << "\n";
+    c.at(i) = ans.at(i)+'0';
+    // std::cout << c.at(i) << "\n";
   }
 
-  return c;
+  BigInt final = reverse_fxn(c);
+
+  // int a_zeros=0;
+  // for(int i=0; i<a_prime.size(); i++){
+  //   a_zeros = a_zeros+(a_prime.at(i)-'0');
+  // }  
+
+  // int b_zeros=0;
+  // for(int i=0; i<a_prime.size(); i++){
+  //   b_zeros = b_zeros+(b_prime.at(i)-'0');
+  // } 
+  // if(a_zeros==0){
+  //   final = "0";
+  // }
+  // if(b_zeros==0){
+  //   final = "0";
+  // }
+
+
+  return final;
 
 
 }
 
-int main(){
-  BigInt a = "111111";
-  BigInt b = "1111111";
-  multiply_int(a,b);
+// int main(){
+//   // BigInt a = "111111";
+//   BigInt a = "0000";
+//   BigInt b = "1111111";
+//     // BigInt a = "543";
+//     // BigInt b = "128";
+//     BigInt z = multiply_int(a,b);
+//     std::cout << z;
 
-}
+//   // BigInt x = reverse_fxn(a);
+//   // BigInt y = reverse_fxn(b);
+
+//   // for(int i=0; i<x.size(); i++){
+//   //   std::cout << x.at(i);
+//   // }
+//   // std::cout << "\n";
+//   // for(int i=0; i<y.size(); i++){
+//   //   std::cout << y.at(i);
+//   // }
+
+// }
