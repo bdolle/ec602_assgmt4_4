48e5693816c84c11b2d5e0a59426f94d246d1755
bdolle <43939940+bdolle@users.noreply.github.com>
2020-10-22 23:20:22 -0400

    Update polyops.cpp

--- polyops.cpp
+++ polyops.cpp
@@ -1,5 +1,7 @@
+// Copyright 2020 Brian Dolle bdolle@bu.edu
+// Copyright 2020 Poras Shroff pshroff4@bu.edu
 #include"polyops.h"
-#include<iostream>
+// #include<iostream>
 #include<vector>
 
 Poly add_poly(const Poly &a, const Poly &b){
@@ -12,15 +14,13 @@ Poly add_poly(const Poly &a, const Poly &b){
   while (y.size()>x.size()){
     x.push_back(0);
   }
+  
   int len_x = static_cast<int>(x.size());
   Poly c(len_x,0);
   for(int i=0; i<len_x; i++){
     c.at(i) = x.at(i)+y.at(i);
   }
 
-  for(int i=0; i<c.size();i++){
-  std::cout << c.at(i) << "\n";
-  }
   return c;
 
 }
@@ -38,25 +38,25 @@ Poly multiply_poly(const Poly & a, const Poly & b){
     }
   }
 
-  for(int i=0; i<c.size();i++){
-  std::cout << c.at(i) << "\n";
-  }
+  // for(int i=0; i<c.size();i++){
+  // std::cout << c.at(i) << "\n";
+  // }
   return c;
 }
 
 
 
-int main(){
-  Poly a(6,0);
-  Poly b(8,0);
+// int main(){
+//   Poly a(6,0);
+//   Poly b(8,0);
 
-  for(int i=0; i<a.size(); i++){
-    a.at(i) = 9*(i+2);
-  }
+//   for(int i=0; i<a.size(); i++){
+//     a.at(i) = 9*(i+2);
+//   }
 
-  for(int i=0; i<b.size(); i++){
-    b.at(i) = 2*(i+1);
-  }
-  add_poly(a,b);
-  multiply_poly(a,b);
-}
+//   for(int i=0; i<b.size(); i++){
+//     b.at(i) = 2*(i+1);
+//   }
+//   add_poly(a,b);
+//   multiply_poly(a,b);
+// }
